fd390b5946357d963c69f02e3c2bea566196ee05
cjlalala <2460311392@qq.com>
2017-04-17 20:10:10 +0800

    Update and rename personalWeb.js to personalWeb_index.js

---
+++ js/personalWeb_index.js
@@ -0,0 +1,27 @@
+function $(id){
+  return document.getElementById(id); 
+}
+//事件绑定函数，兼容浏览器
+function addEvent(dom,type,fn){
+    if(dom.addEventListener){
+        dom.addEventListener(type,fn,false);
+    }
+    else if(dom.attachEvent){
+        dom.attachEvent('on'+type,fn);
+    }
+    else dom['on'+type]=fn;
+}
+window.onload=function(){
+  //导航栏滚动固定
+  window.onscroll=function(){
+    var nav=$('nav'),
+        top=$('top');
+    var imgHeight=document.body.clientHeight||document.documentElement.clientHeight;
+    var target_top=document.body.scrollTop||document.documentElement.scrollTop;
+    target_top >= imgHeight? nav.className = "fixed":nav.className = "";
+    target_top>=imgHeight+50?top.style.display='block':top.style.display='none';
+    addEvent(top,'click',function(){
+        window.scrollTo(0,0);
+    });
+  }
+}
