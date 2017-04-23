d4fa38102bcb56b8375b8f04a811d7983c033fc3
cjlalala <2460311392@qq.com>
2017-04-23 21:59:37 +0800

    Update personalWeb_index.js

--- js/personalWeb_index.js
+++ js/personalWeb_index.js
@@ -10,10 +10,11 @@ function addEvent(dom,type,fn){
 }
 window.onload=function(){
   //导航栏滚动固定
+  var imgHeight;
+  clientW<541?imgHeight=50:imgHeight=document.body.clientHeight||document.documentElement.clientHeight;
   window.onscroll=function(){
     var nav=document.getElementById('nav'),
         top=document.getElementById('top');
-    var imgHeight=document.body.clientHeight||document.documentElement.clientHeight;
     var target_top=document.body.scrollTop||document.documentElement.scrollTop;
     target_top >= imgHeight? nav.className = "fixed":nav.className = "";
     target_top>=imgHeight+50?top.style.display='block':top.style.display='none';
@@ -22,3 +23,16 @@ window.onload=function(){
     });
   }
 }
+var clientW=document.body.clientWidth||document.documentElement.clientWidth;
+var curIndex = 0;
+var arr =['img/indexImg/blog.png','img/indexImg/demo.png',
+'img/indexImg/photo111.png','img/indexImg/word2.png'];
+setInterval(function(){
+  if(clientW>540)return;
+  else{
+    curIndex == arr.length-1? curIndex = 0:curIndex += 1; 
+    document.getElementById("carPic").src = arr[curIndex]; 
+  }
+},2500); 
+   
+
